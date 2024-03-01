afac172a7e24b54f2b4e5b20bfc4adf030cd2f4d
lauramoura98 <lauramoura06@gmail.com>
2024-03-01 16:09:20 -0300

    1.0

---
+++ readme.md
@@ -0,0 +1,9 @@
+**LEGENDA**
+_ENFESTO = Conjunto de folhas de tecido.
+_PEÇAS = Mochilas, Bolsas, Pastas, ETC.
+**REQUISITOS VERSÃO 1.0**
+- A calculadora deverá ser capaz de calcular a quantidade de enfestos necessários, dado uma quantidade de peças por folha e a quantidade total de peças por produção. 
+- A calculadora deverá conter caixas de preenchimento manual para "Quantidade de peças/folha", "Quantidade total/produção" "Metros por enfesto".
+- A calculadora deverá conter, por meio de uma lista Dropdown, as opções de 5,10 e 20 folhas de tecido por enfesto, e a opção "Outros" para preenchimento manual do usuário final.
+- A calculadora deverá mostrar o resultado final do calculo em sua interface, com a opção de copiar o resultado final por meio de recurso clicável.
+- A calculadora deverá conter um botão para apagar o resultado obtido pelo calculo, e começar novamente. 
---
+++ script.js
@@ -0,0 +1,53 @@
+
+var btn = document.querySelector(".btn");
+
+btn.addEventListener("click", resultado);
+
+function resultado(){
+var pcFolha = parseInt(document.querySelector(".pç-folha").value);
+var totalProd = parseInt(document.querySelector(".total-prod").value);
+var folhas = parseInt(document.querySelector("#folhas").value);
+var mts = document.querySelector("#mts-enf").value;
+var resultCol1 = document.querySelector(".result-col-1");
+var resultCol2 = document.querySelector(".result-col-2");
+
+function pontoVirg(mts){
+    const repVirg = mts.replace(",",".");
+    return parseFloat(repVirg);
+}
+
+// Função para arredondar os valores para cima ou para baixo
+    function arredondamento(totalProd) {
+        if (totalProd % 10 < 5) {
+        return Math.floor(totalProd / 10) * 10;
+        } else {
+        return Math.ceil(totalProd / 10) * 10;
+        }
+    }
+
+    //Variáveis para o calculo dos enfestos em decimais
+   const dvs = arredondamento(totalProd)/pcFolha;
+   const totEnf = dvs/folhas;
+
+    if (totEnf < 1){ //Para Numeros com décimos 
+        var umDec = totEnf * 10;
+        resultCol1.innerHTML = `1 enfesto de ${umDec} folhas(${pontoVirg(mts)} MTS)`
+        resultCol2.innerHTML = ``
+    } else{   //Para Numeros com centésimos 
+
+    //Variável para extrair o primeiro valor da decimal
+    const roundRes = Math.floor(totEnf);
+    //variáveis para extrair o segundo valor da decimal, caso houver
+    const SecRes = totEnf - Math.floor(totEnf);
+    var SecResRound = Math.round(SecRes * 10);
+
+    if (SecResRound == 0){
+        resultCol1.innerHTML = `${roundRes} enfestos de ${folhas} folhas(${pontoVirg(mts)} MTS)`;
+        resultCol2.innerHTML = ``
+    }
+        else{
+            resultCol1.innerHTML = `${roundRes} enfestos de ${folhas} folhas(${pontoVirg(mts)} MTS)`;
+            resultCol2.innerHTML = `1 enfesto de ${SecResRound} folhas(${pontoVirg(mts)} MTS)`
+        }
+    }
+    };
