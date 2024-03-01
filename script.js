353205792dc5433c75b435c86c32451f70d5cd5c
lauramoura98 <lauramoura06@gmail.com>
2024-03-01 17:20:34 -0300

    1.0.2

--- script.js
+++ script.js
@@ -7,32 +7,23 @@ var btn = document.querySelector(".btn");
 btn.addEventListener("click", resultado);
 
 pcFolhaInp.addEventListener("keypress", function(event) {
-    // If the user presses the "Enter" key on the keyboard
     if (event.key === "Enter") {
-      // Cancel the default action, if needed
       event.preventDefault();
-      // Trigger the button element with a click
       btn.click();
     }
   });
 
 
   totalProdInp.addEventListener("keypress", function(event) {
-    // If the user presses the "Enter" key on the keyboard
     if (event.key === "Enter") {
-      // Cancel the default action, if needed
       event.preventDefault();
-      // Trigger the button element with a click
       btn.click();
     }
   });
 
   folhasInp.addEventListener("keypress", function(event) {
-    // If the user presses the "Enter" key on the keyboard
     if (event.key === "Enter") {
-      // Cancel the default action, if needed
       event.preventDefault();
-      // Trigger the button element with a click
       btn.click();
     }
   });
